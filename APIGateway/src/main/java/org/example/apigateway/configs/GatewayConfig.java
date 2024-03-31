80eec2baa4e1d190f0df42274f4774eb7fd0a7c6
limbanga <95570833+limbanga@users.noreply.github.com>
2024-04-01 00:49:59 +0700

    done api jwt auth for api gateway

---
+++ APIGateway/src/main/java/org/example/apigateway/configs/GatewayConfig.java
@@ -0,0 +1,31 @@
+package org.example.apigateway.configs;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableHystrix
+public class GatewayConfig {
+    private final AuthenticationFilter filter;
+
+    public GatewayConfig(AuthenticationFilter filter) {
+        this.filter = filter;
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("ListingService", r -> r.path("/api/listing/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://ListingService"))
+                .route("SecurityService", r -> r.path("/auth/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://SecurityService"))
+                .build();
+    }
+}
---
+++ APIGateway/src/main/java/org/example/apigateway/configs/RouterValidator.java
@@ -0,0 +1,21 @@
+package org.example.apigateway.configs;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class RouterValidator {
+
+    public static final List<String> openEndpoints = List.of(
+            "/auth/register",
+            "/auth/**",
+            "/api/listing/**"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openEndpoints.stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
